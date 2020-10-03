f8b9692807919b3108776458bb8c72722fabd751
Martin Monperrus <martin.monperrus@gnieh.org>
2020-10-03 10:18:01 +0200

    @monperrus at work

---
+++ src/main/java/fr/spoonlabs/spfl/core/TestTuple.java
@@ -0,0 +1,41 @@
+package fr.spoonlabs.spfl.core;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 
+ * @author Matias Martinez
+ *
+ */
+public class TestTuple {
+	public String testClassToBeAmplified;
+
+	public List<String> testMethodsToBeAmplified;
+
+	public Map<String, Object> metadata = new HashMap<String, Object>();
+
+	public TestTuple(String testClassToBeAmplified, List<String> testMethodsToBeAmplified) {
+		super();
+		this.testClassToBeAmplified = testClassToBeAmplified;
+		this.testMethodsToBeAmplified = testMethodsToBeAmplified;
+	}
+
+	public String getTestClassToBeAmplified() {
+		return testClassToBeAmplified;
+	}
+
+	public void setTestClassToBeAmplified(String testClassToBeAmplified) {
+		this.testClassToBeAmplified = testClassToBeAmplified;
+	}
+
+	public List<String> getTestMethodsToBeAmplified() {
+		return testMethodsToBeAmplified;
+	}
+
+	public void setTestMethodsToBeAmplified(List<String> testMethodsToBeAmplified) {
+		this.testMethodsToBeAmplified = testMethodsToBeAmplified;
+	}
+
+}
---
+++ src/test/java/fr/spoonlabs/spfl/TestDetector.java
@@ -0,0 +1,50 @@
+package fr.spoonlabs.spfl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import eu.stamp_project.testrunner.test_framework.TestFramework;
+import fr.spoonlabs.spfl.core.TestTuple;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtType;
+import spoon.reflect.factory.Factory;
+
+/**
+ * 
+ * @author Matias Martinez
+ *
+ */
+public class TestDetector {
+	TestFramework testFramework;
+
+	public TestDetector() {
+
+	}
+
+	public List<TestTuple> findTest(Factory factory) {
+		TestFramework.init(factory);
+		this.testFramework = TestFramework.get();
+
+		List<TestTuple> tuples = new ArrayList();
+
+		List<CtType<?>> modelclassesOfTest = this.testFramework.getAllTestClasses();
+		for (CtType<?> type : modelclassesOfTest) {
+
+			if (type.isAbstract()) {
+				continue;
+			}
+
+			List<CtMethod<?>> methodsTest = this.testFramework.getAllTest(type);
+
+			List<String> methodTestNames = methodsTest.stream().map(e -> e.getSimpleName())
+					.collect(Collectors.toList());
+
+			TestTuple tuple = new TestTuple(type.getQualifiedName(), methodTestNames);
+
+			tuples.add(tuple);
+		}
+		return tuples;
+	}
+
+}
