1902222c0ca4e25fa10358ca136c9618928b098f
André Silva <andre15andre@hotmail.com>
2021-06-16 15:59:08 +0200

    Improve CI (#16)
    * Remove target files from repo. Update CI.
    
    Signed-off-by: André Silva <andre15andre@hotmail.com>
    
    * Specify env options
    
    Signed-off-by: André Silva <andre15andre@hotmail.com>
    
    * Make scripts executable
    
    Signed-off-by: André Silva <andre15andre@hotmail.com>
    
    * Enable codecov
    
    Signed-off-by: André Silva <andre15andre@hotmail.com>
    
    * Add CI badges to README
    
    Signed-off-by: André Silva <andre15andre@hotmail.com>
    
    * Make code_coverage script executable
    
    Signed-off-by: André Silva <andre15andre@hotmail.com>
    
    * Update NPE test
    
    Signed-off-by: André Silva <andre15andre@hotmail.com>
    
    * Disable codecov comments
    
    Signed-off-by: André Silva <andre15andre@hotmail.com>
    
    * Improve README header
    
    Signed-off-by: André Silva <andre15andre@hotmail.com>

---
+++ .travis/execute_tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Script that executes tests
+
+mvn clean test -B
---
+++ .travis/install_examples.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Script that generates the required target files for each example project
+
+mvn clean test -DskipTests -B -f examples/exampleFL1/FLtest1/
+mvn clean test -DskipTests -B -f examples/exampleFL2/FLtest1/
+mvn clean test -DskipTests -B -f examples/exampleFL3/FLtest1/
