021cd01d1823cfc23e5c25afd2a7ee1c15626f65
ashraf04066 <ashraf.uddin04066@gmail.com>
2023-05-23 17:16:40 -0500

    tarantula_added

--- src/main/java/fr/spoonlabs/flacoco/localization/spectrum/formulas/OchiaiFormula.java
+++ src/main/java/fr/spoonlabs/flacoco/localization/spectrum/formulas/OchiaiFormula.java
@@ -8,11 +8,28 @@ public class OchiaiFormula implements Formula {
 	public double compute(int nPassingNotExecuting, int nFailingNotExecuting, int nPassingExecuting,
 			int nFailingExecuting) {
 
-		if ((nFailingExecuting + nPassingExecuting == 0) || (nFailingExecuting + nFailingNotExecuting == 0)) {
+//		if ((nFailingExecuting + nPassingExecuting == 0) || (nFailingExecuting + nFailingNotExecuting == 0)) {
+//			return 0;
+//		}
+
+		if ((nFailingExecuting + nFailingNotExecuting == 0) || (nPassingExecuting+nPassingNotExecuting ==0) || ((nFailingExecuting/(nFailingExecuting+nFailingNotExecuting)) + (nPassingExecuting/(nPassingExecuting+nPassingNotExecuting))) == 0) {
 			return 0;
 		}
-		return nFailingExecuting
-				/ (Math.sqrt((nFailingExecuting + nFailingNotExecuting) * (nFailingExecuting + nPassingExecuting)));
+
+//		if (( nPassingExecuting + ((nFailingExecuting+nFailingNotExecuting)-(nFailingExecuting))) == 0) {
+//			return 0;
+//		}
+
+//		return nFailingExecuting
+//				/ (Math.sqrt((nFailingExecuting + nFailingNotExecuting) * (nFailingExecuting + nPassingExecuting)));
+
+		return ((nFailingExecuting/(nFailingExecuting+nFailingNotExecuting))
+				/ ((nFailingExecuting/(nFailingExecuting+nFailingNotExecuting)) + (nPassingExecuting/(nPassingExecuting+nPassingNotExecuting))));
+
+
+
+//		return (nFailingExecuting / ( nPassingExecuting + ((nFailingExecuting+nFailingNotExecuting)-(nFailingExecuting))));
+
 	}
 
 }
