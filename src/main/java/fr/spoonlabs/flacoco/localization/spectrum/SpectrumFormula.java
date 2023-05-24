7ccd693846e442e07ae0658c6505bc13f32e33e5
André Silva <andre15andre@hotmail.com>
2023-05-24 09:00:57 +0200

    add tarantula

--- src/main/java/fr/spoonlabs/flacoco/localization/spectrum/SpectrumFormula.java
+++ src/main/java/fr/spoonlabs/flacoco/localization/spectrum/SpectrumFormula.java
@@ -2,10 +2,12 @@ package fr.spoonlabs.flacoco.localization.spectrum;
 
 import fr.spoonlabs.flacoco.localization.spectrum.formulas.Formula;
 import fr.spoonlabs.flacoco.localization.spectrum.formulas.OchiaiFormula;
+import fr.spoonlabs.flacoco.localization.spectrum.formulas.TarantulaFormula;
 
 public enum SpectrumFormula {
 
-	OCHIAI(new OchiaiFormula());
+	OCHIAI(new OchiaiFormula()),
+	TARANTULA(new TarantulaFormula());
 
 	private final Formula formula;
 
